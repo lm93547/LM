10e256b875fbf3265ce3632435ebef85e7e6c178
Lucas Martin <lucasmartin045@gmail.com>
2022-11-07 23:36:34 +0800

    Adding navLinks & socialLinks constants

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,12 @@
+import { GITHUB_URL, LINKEDIN_URL } from "./userInfo"
+
+export const navLinks = [
+    {title: "About", url: "/about"},
+    {title: "My Work", url: '/work'},
+    {title: "Resume", url: ''}
+]
+
+export const socialLinks = [
+    {title: "Github", url: GITHUB_URL},
+    {title: "LinkedIn", url: LINKEDIN_URL}
+]
