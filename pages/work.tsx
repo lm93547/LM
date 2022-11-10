ee434806b5d875e62952076966c67035db5a3f42
Lucas Martin <lucasmartin045@gmail.com>
2022-11-10 23:48:56 +0800

    Add placeholder page for my work

---
+++ pages/work.tsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+import Layout from "../src/components/Layout";
+
+
+export default function Work(){
+    return (
+        <Layout>
+            <Flex h="full" >
+                My Work
+            </Flex>
+        </Layout>
+    )
+}
