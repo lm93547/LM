5bea040cf15aa4e65650f2b3cb7e8a513ef723f6
Lucas Martin <lucasmartin045@gmail.com>
2022-11-10 23:47:57 +0800

    Add transition wrapper from framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider} from "@chakra-ui/react";
-import {theme} from '../styles/theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme} >
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AnimatePresence
+      exitBeforeEnter
+      initial={false}
+      onExitComplete={() => window.scrollTo(0, 0)}
+    >
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AnimatePresence>
   );
 }
