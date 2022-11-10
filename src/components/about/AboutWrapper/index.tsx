887f5e47a6b2d8d19eb540527608b2e9ea72b273
Lucas Martin <lucasmartin045@gmail.com>
2022-11-10 23:43:35 +0800

    Abstract about component into a wrapper w background image & avatar

---
+++ src/components/about/AboutWrapper/index.tsx
@@ -0,0 +1,43 @@
+import { Flex, Avatar, Image, useBreakpointValue } from "@chakra-ui/react";
+import React from "react";
+
+type Props = {
+    children: JSX.Element
+    avatar: `/assets/${string}`
+    aboutHeader: `/assets/${string}`
+};
+
+const AboutWrapper = ({children, avatar, aboutHeader}: Props) => {
+  return (
+    <Flex h="full" w="full" alignItems="center" justifyContent="center">
+      <Flex
+        maxW="2xl"
+        bgColor="gray.700"
+        rounded="2xl"
+        direction="column"
+        m={useBreakpointValue({ base: "6", md: "2" })}
+      >
+        <Image
+          h={"160px"}
+          w={"full"}
+          src={aboutHeader}
+          objectFit={"cover"}
+          alt="about-header"
+          roundedTop="2xl"
+        />
+        <Flex justify={"center"} mt={-14}>
+          <Avatar
+            size={"2xl"}
+            src={avatar}
+            css={{
+              border: "2px solid white",
+            }}
+          />
+        </Flex>
+        {children}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default AboutWrapper;
