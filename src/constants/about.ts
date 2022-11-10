e9922b9f2bc0901fbe78356c1ecbfa7ef18ad205
Lucas Martin <lucasmartin045@gmail.com>
2022-11-10 23:40:37 +0800

    adding about constant

---
+++ src/constants/about.ts
@@ -0,0 +1,3 @@
+export const ABOUT = "Driven. Motivated. Self taught software engineer hailing from Manchester, UK. Working in fast paced startup teams, creating accessible and beautiful software. Crafting efficient business logic & delivering value."
+
+export const ABOUT_TITLE = "Me."
