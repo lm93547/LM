46972d92c57791db9b40118f056cb91aa323a6a5
Lucas Martin <lucasmartin045@gmail.com>
2022-11-10 23:39:40 +0800

    add struct URL & Bio to userInfo constants

--- src/constants/userInfo.ts
+++ src/constants/userInfo.ts
@@ -8,7 +8,8 @@ export const GITHUB_URL = "https://github.com/lm93547"
 export const LINKEDIN_URL = "https://www.linkedin.com/in/lucas-martin-459449174"
 
 export const STRUCT = "Struct Finance"
+export const STRUCT_URL = "https://github.com/struct-defi"
 
 export const TAGLINE = "Fullstack Software Developer"
 
-export const BIO = "Typescript, React, Python, Golang, Blockchain, Geospatial and much more. Hardworking and focused on detail. "
+export const BIO = "Typescript, React, Python, Golang, Blockchain, Geospatial and much more. Hardworking and focused on detail."
