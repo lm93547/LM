9f60cebec266485762290da37b6441dfdc1486b4
Lucas Martin <lucasmartin045@gmail.com>
2022-11-10 23:42:08 +0800

    add framer motion motion div & variants for enter, mount and exit

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,33 +1,48 @@
-import { Flex } from '@chakra-ui/react'
-import React from 'react'
-import Footer from '../Footer'
-import Navbar from '../Navbar'
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import Footer from "../Footer";
+import Navbar from "../Navbar";
+import { motion } from 'framer-motion'
 
 type Props = {
-    children: any
-}
+  children: any;
+};
 
-const Layout = ({children}: Props) => {
+const variants = {
+  hidden: { opacity: 0, y: 0, scale: 0.9 },
+  enter: { opacity: 1, x: 0, y: 0, scale: 1 },
+  exit: { opacity: 0, x: 0, scale: 0.9 },
+};
+
+const Layout = ({ children }: Props) => {
   return (
-    <Flex
-      w={"full"}
-      h={"100vh"}
-      backgroundImage={"/assets/backgrounds/mcr-black.png"}
-      backgroundSize={"cover"}
-      backgroundPosition={"center center"}
+    <motion.main
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      variants={variants}
+      transition={{ type: "linear" }}
     >
       <Flex
         w={"full"}
         h={"100vh"}
-        bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
-        direction="column"
+        backgroundImage={"/assets/backgrounds/mcr-black.png"}
+        backgroundSize={"cover"}
+        backgroundPosition={"center center"}
       >
-        <Navbar />
-        {children}
-        <Footer />
+        <Flex
+          w={"full"}
+          h={"100vh"}
+          bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+          direction="column"
+        >
+          <Navbar />
+          {children}
+          <Footer />
+        </Flex>
       </Flex>
-    </Flex>
-  )
-}
+    </motion.main>
+  );
+};
 
-export default Layout
+export default Layout;
