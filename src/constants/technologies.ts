f31db05769e76d727b667dae104069f2d26c4b17
Lucas Martin <lucasmartin045@gmail.com>
2022-11-10 23:40:28 +0800

    add technologies array w titles & icons

---
+++ src/constants/technologies.ts
@@ -0,0 +1,11 @@
+import { SiTypescript, SiReact, SiGoland, SiJavascript, SiPython } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+
+export const technologies = [
+    {name: "Typescript", icon: SiTypescript},
+    {name: "React", icon: SiReact},
+    {name: "NextJS", icon: TbBrandNextjs},
+    {name: "Golang", icon: SiGoland},
+    {name: "Javascript", icon: SiJavascript},
+    {name: "Python", icon: SiPython}
+]
