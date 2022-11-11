6782cfb3b3ec5024c401e8e2eafa15ea40fccbdb
Lucas Martin <lucasmartin045@gmail.com>
2022-11-11 17:55:59 +0800

    add meta tag & next head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,24 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AnimatePresence
-      exitBeforeEnter
-      initial={false}
-      onExitComplete={() => window.scrollTo(0, 0)}
-    >
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </AnimatePresence>
+    <>
+      <Head>
+        <title>Lucas Martin</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <AnimatePresence
+        exitBeforeEnter
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </AnimatePresence>
+    </>
   );
 }
